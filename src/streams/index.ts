462be77e652d90c09c9e6e8c76c3cf1d95028076
alexanchek <58663068+alexanchek@users.noreply.github.com>
2023-07-25 15:07:06 +0300

    upd: added ultra service

--- src/streams/index.ts
+++ src/streams/index.ts
@@ -3,10 +3,10 @@ import { IStream } from "../models/stream.interface";
 import radioMaximum from './radioMaximum.jpg';
 import radioUltra from './radioUltra.jpg';
 import radioGelosa from './radioGelosa.jpg';
-import { getGelosaInfo, getMaximumInfo } from "../services/trackinfo.service";
+import { getGelosaInfo, getMaximumInfo, getUltraInfo } from "../services/trackinfo.service";
 
 export const streams: IStream[] = [
-  { name: 'Радио Ультра', url: 'https://nashe1.hostingradio.ru/ultra-256', cover: radioUltra, extra: 'ULTRA' },
+  { name: 'Радио Ультра', url: 'https://nashe1.hostingradio.ru/ultra-256', cover: radioUltra, extra: 'ULTRA', titleUrl: getUltraInfo },
   { name: 'Радио Максимум', url: 'https://maximum.hostingradio.ru/maximum96.aacp', cover: radioMaximum, extra: '103,7', titleUrl: getMaximumInfo, },
   { name: 'Radio Gelosa', url: 'https://ice01.fluidstream.net/gelosa?FLID=1&type=.aac', cover: radioGelosa, extra: 'Gelosa', titleUrl: getGelosaInfo },
   
